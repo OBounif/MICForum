ad49482348b69ebe24a68d01ae039863eec787c9
Fr3eX <bounifomar44@hotmail.com>
2021-09-20 22:48:49 +0100

    Adding some util functions for multipart request parsing

--- src/main/java/com/bounifomar/micforum/business/blimplementations/utility/RequestParsUtility.java
+++ src/main/java/com/bounifomar/micforum/business/blimplementations/utility/RequestParsUtility.java
@@ -1,6 +1,11 @@
 package com.bounifomar.micforum.business.blimplementations.utility;
 
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 
 public class RequestParsUtility {
 		
@@ -15,5 +20,60 @@ public class RequestParsUtility {
 				return fieldValue.trim();
 	}
 	
+	public static String getPartStream_name(HttpServletRequest request,String PART_FIELD,Map<String,String> errors)
+	{
+		try
+		{
+			Part cover_pic = request.getPart(PART_FIELD);
+			
+			
+			String filename = getFileName(cover_pic);
+			
+			if(filename !=null && !filename.isEmpty())
+			{
+				/*
+				 *  -ANTIBUG for Internet explorer which sends the absolute path of the file in the client FS.
+				 * */
+				String filename_ie = filename.substring( filename.lastIndexOf( '/' ) + 1 )
+                        .substring( filename.lastIndexOf( '\\' ) + 1 );
+				
+		
+				return filename_ie;
+			}
+			
+		}
+		catch ( IllegalStateException e) {
+			errors.put(PART_FIELD, "La taille de l'image ne doit pas dépasser 2MB.");
+			e.printStackTrace();
+
+		}
+		catch (IOException e) {
+			errors.put(PART_FIELD, "Une erreur inattendue est survenu réessayer ultérieurement");
+			e.printStackTrace();
+
+
+		}
+		catch (ServletException e) {
+			errors.put(PART_FIELD, "Ce type de requête n'est pas supporté.");
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+	}
 	
-}
+	
+	private static String getFileName(Part part)
+	{
+		if(part == null)
+			return null;
+		
+		for(String contentD : part.getHeader("content-disposition").split(";"))
+		{
+			if(contentD.trim().startsWith("filename"))
+				return contentD.substring(contentD.indexOf("=")+1).trim().replace("\"", "");
+		}
+		
+		return null;
+	}
+ }
